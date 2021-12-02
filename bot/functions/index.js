1182deb44323e0a34175a60014a13b67620f4a88
Mattareeya Rach-M <matzeeya@gmail.com>
2021-12-03 01:14:39 +0700

    upload photo

--- bot/functions/index.js
+++ bot/functions/index.js
@@ -1,82 +1,112 @@
-// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
-// for Dialogflow fulfillment library docs, samples, and to report issues
-'use strict';
-
 const functions = require('firebase-functions')
-const request = require('request-promise')
 
 // สำหรับการเข้าถึง Cloud Storage
-const admin = require('firebase-admin')
-admin.initializeApp()
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+// สำหรับ network requests
+const axios = require('axios');
 
 // สำหรับสร้าง public url ใน Cloud Storage
-const UUID = require('uuid-v4')
+const UUID = require("uuid-v4");
+
+// เชื่อมต่อ firebase
+var config = require('./config.js');
 
 // สำหรับจัดการไฟล์
-const path = require('path')
-const os = require('os')
-const fs = require('fs')
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
-// เชื่อมต่อ Dialogflow
-const {WebhookClient, Payload} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
+const LINE_MESSAGING_API = "https://api.line.me/v2/bot";
+const LINE_CONTENT_API = "https://api-data.line.me/v2/bot/message";
+const LINE_HEADER = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${config.accessToken}`
+};
 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
-
-exports.uploadPhoto = functions.https.onRequest((request, response) => {
-
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-
-  async function upload(req, res){
-    //agent.add("Photo")
-    let event = req.body.events[0]
-    if (event.type === 'message' && event.message.type === 'image') {
-      // เรียกฟังก์ชัน upload เมื่อเข้าเงื่อนไข
-      //let urls = await upload(event)
-
-      // reply ตัว URL ที่ได้กลับไปยังห้องแชท
-      await reply(event.replyToken, { type: "text", text: "Photo" })
-    }
-  }
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('upload-photo', upload);
-  agent.handleRequest(intentMap);
-
-  //let event = req.body.events[0]
-  //if (event.type === 'message' && event.message.type === 'image') {
+exports.fulfillment = functions.https.onRequest(async(req, res) => {
+  const event = req.body.events[0];
+  if (event.type === 'message' && event.message.type === 'image') {
     // เรียกฟังก์ชัน upload เมื่อเข้าเงื่อนไข
-    //let urls = await upload(event)
-
+    const urls = await upload(event);
+    
     // reply ตัว URL ที่ได้กลับไปยังห้องแชท
-    //await reply(event.replyToken, { type: "text", text: urls })
-  //}
+    await reply(
+      event.replyToken,
+      {
+        "type": "flex",
+        "altText": "Flex Message",
+        "contents": {
+          "type": "bubble",
+          "hero": {
+            "type": "image",
+            "url": urls.original,
+            "size": "full",
+            "aspectRatio": "1:1",
+            "aspectMode": "cover"
+          },
+          "footer": {
+            "type": "box",
+            "layout": "horizontal",
+            "spacing": "md",
+            "contents": [
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "Original",
+                  "uri": urls.original
+                },
+                "style": "secondary"
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "Thumb",
+                  "uri": urls.thumb
+                },
+                "style": "primary"
+              }
+            ]
+          }
+        }
+      }
+    );
+  }
+  return res.end();
+});
 
-})
-
-/*const upload = async (event) => {
-  // ดาวน์โหลด binary จาก LINE 
-  const LINE_CONTENT_API = 'https://api-data.line.me/v2/bot/message'
-  let url = `${LINE_CONTENT_API}/${event.message.id}/content`
-  let buffer = await request.get({
+const reply = (replyToken, payload) => {
+  axios({
+    method: "post",
+    url: `${LINE_MESSAGING_API}/message/reply`,
     headers: LINE_HEADER,
-    uri: url,
-    encoding: null // กำหนดเป็น null เพื่อให้ได้ binary ที่สมบูรณ์
+    data: JSON.stringify({
+      replyToken: replyToken,
+      messages: [payload]
+    })
   })
+};
 
-  // สร้างไฟล์ temp ใน local โดยใช้ timestamp ที่ได้จาก webhook เป็นชื่อไฟล์
-  let filename = `${event.timestamp}.jpg`
-  let tempLocalFile = path.join(os.tmpdir(), filename)
-  await fs.writeFileSync(tempLocalFile, buffer)
+const upload = async(event) => {
+  const url = `${LINE_CONTENT_API}/${event.message.id}/content`;
+  const buffer = await axios({
+    method: "get",
+    headers: LINE_HEADER,
+    url: url,
+    responseType: "arraybuffer"
+  });
 
-  // generate ตัว uuid
-  let uuid = UUID()
-  
-  // อัพโหลดไฟล์ขึ้น Cloud Storage
-  let bucket = admin.storage().bucket()
-  let file = await bucket.upload(tempLocalFile, {
+  const filename = `${event.timestamp}.jpg`;
+  const tempLocalFile = path.join(os.tmpdir(), filename);
+  await fs.writeFileSync(tempLocalFile, buffer.data);
+
+  const uuid = UUID()
+
+  const bucket = admin.storage().bucket()
+  const file = await bucket.upload(tempLocalFile, {
     // กำหนด path ในการเก็บไฟล์แยกเป็นแต่ละ userId
     destination: `photos/${event.source.userId}/${filename}`,
     metadata: {
@@ -86,23 +116,11 @@ exports.uploadPhoto = functions.https.onRequest((request, response) => {
       }
     }
   })
-  
-  // ลบไฟล์ temp เมื่ออัพโหลดเรียบร้อย
   fs.unlinkSync(tempLocalFile)
-
-  // วิธีลัดในการสร้าง download url ขึ้นมา
-  let prefix = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o`
-  let suffix = `alt=media&token=${uuid}`
-  return `${prefix}/${encodeURIComponent(file[0].name)}?${suffix}`
-}
-
-const reply = (replyToken, payload) => {
-  request.post({
-    uri: `${LINE_MESSAGING_API}/message/reply`,
-    headers: LINE_HEADER,
-    body: JSON.stringify({
-      replyToken: replyToken,
-      messages: [payload]
-    })
-  })
-}*/
+  let prefix = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o`;
+  let suffix = `alt=media&token=${uuid}`;
+  return {
+    original: `${prefix}/${encodeURIComponent(file[0].name)}?${suffix}`,
+    thumb: `${prefix}/photos${encodeURIComponent(`/${event.source.userId}/thumbs/${event.timestamp}_500x500.jpg`)}?${suffix}`
+  };
+};
