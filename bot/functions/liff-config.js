a9008870b770c8123ac4ad05567c2e9fbafa3a22
Mattareeya Rach-M <matzeeya@gmail.com>
2021-12-12 21:42:53 +0700

    add liff url

--- bot/functions/liff-config.js
+++ bot/functions/liff-config.js
@@ -1,5 +1,6 @@
 const config = {
-    LIFF_URL: 'https://liff.line.me/1656639524-loJjd1JV'
+    LIFF_URL: 'https://liff.line.me/1656639524-loJjd1JV',
+    LIFF_ID: '1656639524-loJjd1JV'
   };
   
   module.exports = config;
