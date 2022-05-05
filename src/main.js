ddb7f739327f05e6358d61a7942bb273ad17f1b3
matzeeya <matzeeya@gmail.com>
2022-05-05 15:48:35 +0700

    i buefy

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
-//import App from './App.vue'
-//import router from './router'
-//import Hello from './components/HelloWorld.vue'
-import page from './views/addUser.vue'
+import App from './App.vue'
+import router from './router'
+import Buefy from 'buefy'
 
 Vue.prototype.$liff = window.liff
 Vue.config.productionTip = false
 
+Vue.use(Buefy)
+
 new Vue({
-  //router,
-  render: h => h(page),
+  router,
+  render: h => h(App),
 }).$mount('#app')
