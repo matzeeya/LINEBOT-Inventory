6d15f3a485aaa450a0640f670046f1eda2dd1464
matzeeya <matzeeya@gmail.com>
2022-07-05 13:54:07 +0700

    edit liffID

--- bot/functions/liff-config.js
+++ bot/functions/liff-config.js
@@ -1,9 +1,22 @@
-function LiffID(){
+function registerLiffID(){
   return "1656639524-loJjd1JV"
 }
 
-function LiffURL(path){
-  return "https://liff.line.me/1656639524-loJjd1JV/" + path
+function registerLiffURL(){
+  return "https://liff.line.me/1656639524-loJjd1JV/"
 }
 
-module.exports={ LiffID,LiffURL};
+function searchLiffID(){
+  return "1657274677-R1eXYeg5"
+}
+
+function searchLiffURL(){
+  return "https://liff.line.me/1657274677-R1eXYeg5"
+}
+
+module.exports={ 
+  registerLiffID,
+  registerLiffURL,
+  searchLiffID,
+  searchLiffURL
+};
