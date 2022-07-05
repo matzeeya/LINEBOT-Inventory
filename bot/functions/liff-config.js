f74ecd91bce4fa649e40df16c26391823e20b5c2
matzeeya <matzeeya@gmail.com>
2022-07-05 14:03:05 +0700

    edit liff

--- bot/functions/liff-config.js
+++ bot/functions/liff-config.js
@@ -1,22 +1,9 @@
-function registerLiffID(){
+function LiffID(){
   return "1656639524-loJjd1JV"
 }
 
-function registerLiffURL(){
-  return "https://liff.line.me/1656639524-loJjd1JV/"
+function LiffURL(path){
+  return "https://liff.line.me/1656639524-loJjd1JV/" + path
 }
 
-function searchLiffID(){
-  return "1657274677-R1eXYeg5"
-}
-
-function searchLiffURL(){
-  return "https://liff.line.me/1657274677-R1eXYeg5"
-}
-
-module.exports={ 
-  registerLiffID,
-  registerLiffURL,
-  searchLiffID,
-  searchLiffURL
-};
+module.exports={ LiffID,LiffURL};
