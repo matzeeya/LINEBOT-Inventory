0cc29f251201e7b5912e8c8c9c920b857ec376e5
matzeeya <matzeeya@gmail.com>
2022-07-06 15:38:29 +0700

    liff

--- bot/functions/liff-config.js
+++ bot/functions/liff-config.js
@@ -1,19 +1,9 @@
-const LiffID = '1657280459-K8l6ZJWY'
-const LiffUrl = 'https://liff.line.me/'+ LiffID
-const registerLiffID = '1656639524-loJjd1JV'
-const registerLiffUrl = 'https://liff.line.me/'+ registerLiffID
-const viewLiffID = '1656639524-loJjd1JV'
-const viewLiffUrl = 'https://liff.line.me/'+ viewLiffID
-const searchLiffID = '1657274677-R1eXYeg5'
-const searchLiffUrl = 'https://liff.line.me/'+ searchLiffID
-
-module.exports={ 
-  LiffID,
-  LiffUrl,
-  registerLiffID,
-  registerLiffUrl,
-  viewLiffID,
-  viewLiffUrl,
-  searchLiffID,
-  searchLiffUrl
+function LiffID(){
+  return "1657280459-K8l6ZJWY"
 }
+
+function LiffURL(path){
+  return "https://liff.line.me/1657280459-K8l6ZJWY" + path
+}
+
+module.exports={ LiffID,LiffURL};
